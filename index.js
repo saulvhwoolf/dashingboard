1bf326441983b1405a2ad1f8b4eec40fecbb012b
saulvhwoolf <saulvhwoolf@gmail.com>
2021-04-14 19:21:19 -0500

    public branch

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-//setting middleware
-app.use(express.static('public'));
-
-console.log("application running at localhost:5000")
-
-var server = app.listen(5000);
